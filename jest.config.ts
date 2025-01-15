b428ec9cf66a4b2f5c10669c6804222b32329f48
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2025-01-15 10:44:54 +0700

    Set up Jest for testing

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+  verbose: true
+};
+
+export default config;
